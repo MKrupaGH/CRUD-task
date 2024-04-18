cc4821c07d261e5b3ad42d5e9c76cd0eafc60694
Marek <marekkrupa03@gmail.com>
2024-04-18 13:24:34 +0200

    validation error moved to template

--- angular-crud-campaigns/src/app/components/add-edit-campaign/add-edit-campaign.component.ts
+++ angular-crud-campaigns/src/app/components/add-edit-campaign/add-edit-campaign.component.ts
@@ -1,15 +1,12 @@
-import { Component, Inject, OnInit, inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
-import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { CommonModule } from '@angular/common';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import {
   FormControl,
   NonNullableFormBuilder,
@@ -23,7 +20,8 @@ import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 import { IDropdownSettings } from 'ng-multiselect-dropdown';
 import { CampaignsService } from '../../services/campaigns.service';
 import { CoreService } from '../../core/core.service';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Campaign } from '../../models/campaign.model';
+import { BudgetService } from '../../services/budget.service';
 enum Fields {
   NAME = 'name',
   STATUS = 'status',
@@ -52,21 +50,19 @@ interface FormType {
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatDatepickerModule,
     MatNativeDateModule,
     MatRadioModule,
     MatSelectModule,
     CommonModule,
-    MatSliderModule,
     ReactiveFormsModule,
     CommonModule,
-    MatAutocompleteModule,
     NgMultiSelectDropDownModule,
   ],
   templateUrl: './add-edit-campaign.component.html',
   styleUrl: './add-edit-campaign.component.scss',
 })
 export class AddEditCampaignComponent implements OnInit {
+  //budget!: number;
   campaignForm = this.fb.group<FormType>({
     [Fields.NAME]: this.fb.control('', [Validators.required]),
     [Fields.STATUS]: this.fb.control('', [Validators.required]),
@@ -75,7 +71,10 @@ export class AddEditCampaignComponent implements OnInit {
       Validators.required,
       Validators.min(20),
     ]),
-    [Fields.CAMPAIGNFUND]: this.fb.control(0, [Validators.required]),
+    [Fields.CAMPAIGNFUND]: this.fb.control(0, [
+      Validators.required,
+      //Validators.max(this.budget),
+    ]),
     [Fields.TOWN]: this.fb.control('', [Validators.required]),
     [Fields.RADIUS]: this.fb.control(0, [Validators.required]),
   });
@@ -98,74 +97,77 @@ export class AddEditCampaignComponent implements OnInit {
     private campaignsService: CampaignsService,
     private coreService: CoreService,
     private dialogRef: MatDialogRef<AddEditCampaignComponent>,
+    private budgetService: BudgetService,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
 
   ngOnInit(): void {
     this.campaignForm.patchValue(this.data);
+    // this.budgetService
+    //   .getBudget()
+    //   .pipe(tap((budget) => (this.budget = budget)));
   }
 
   onFormSubmit() {
-    console.log(this.campaignForm.value);
     if (this.campaignForm.valid) {
       if (this.data) {
-        this.campaignsService
-          .updateCampaignById(this.data.id, this.campaignForm.getRawValue())
-          .subscribe({
-            next: (val: any) => {
-              this.campaignsService.campaignsState.next(
-                [...this.campaignsService.campaignsState.value].map(
-                  (campaign) => {
-                    if (campaign.id === val.id) {
-                      campaign = val;
-                    }
-                    return campaign;
-                  }
-                )
-              );
-              this.coreService.openSnackBar(
-                'Employee updated successfully',
-                'Updated'
-              );
-              this.dialogRef.close(true);
-            },
-            error: (err) => {
-              console.error(err);
-            },
-          });
+        this.updateCampaign();
       } else {
-        this.campaignsService
-          .addCampaign(this.campaignForm.getRawValue())
-          .subscribe({
-            next: (val: any) => {
-              console.log(val);
-              this.campaignsService.campaignsState.next([
-                ...this.campaignsService.campaignsState.value,
-                val,
-              ]);
-              console.log(this.campaignsService.campaignsState.value);
-
-              this.coreService.openSnackBar(
-                'Employee added successfully',
-                'Added'
-              );
-              this.dialogRef.close(true);
-            },
-            error: (err) => {
-              console.error(err);
-            },
-          });
+        this.addCampaign();
       }
     }
   }
 
-  showMinimalErrors() {
-    const bidAmount = this.campaignForm.get('bidAmount');
-    if (bidAmount?.touched && !bidAmount.valid) {
-      if (bidAmount.errors?.['min']) {
-        return 'Bid should be minimum 20$';
-      }
-    }
-    return;
+  private updateCampaign() {
+    this.campaignsService
+      .updateCampaignById(this.data.id, this.campaignForm.getRawValue())
+      .subscribe({
+        next: (campaign: Campaign) => {
+          this.updateCampaignState(campaign);
+          this.showSuccessSnackBar('Employee updated successfully', 'Updated');
+          this.closeDialog();
+        },
+        error: (err) => this.handleError(err),
+      });
+  }
+
+  private addCampaign() {
+    this.campaignsService
+      .addCampaign(this.campaignForm.getRawValue())
+      .subscribe({
+        next: (campaign: Campaign) => {
+          this.addCampaignToState(campaign);
+          this.showSuccessSnackBar('Employee added successfully', 'Added');
+          this.closeDialog();
+        },
+        error: (err) => this.handleError(err),
+      });
+  }
+
+  private updateCampaignState(newCampaign: Campaign) {
+    this.campaignsService.campaignsState.next(
+      this.campaignsService.campaignsState.value.map((campaign) =>
+        campaign.id === newCampaign.id ? newCampaign : campaign
+      )
+    );
+  }
+
+  private addCampaignToState(newCampaign: Campaign) {
+    this.campaignsService.campaignsState.next([
+      ...this.campaignsService.campaignsState.value,
+      newCampaign,
+    ]);
+  }
+
+  private showSuccessSnackBar(message: string, action: string) {
+    this.coreService.openSnackBar(message, action);
+  }
+
+  private closeDialog() {
+    this.dialogRef.close(true);
+  }
+
+  private handleError(error: any) {
+    console.error(error);
   }
 }
